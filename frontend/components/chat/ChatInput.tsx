471dabcba07ff5578c8c278d243f610308f4a2e0
Weng Ti <wengti@hotmail.com>
2026-05-19 17:31:26 +0800

    show error msg

--- frontend/components/chat/ChatInput.tsx
+++ frontend/components/chat/ChatInput.tsx
@@ -6,6 +6,7 @@ import { MessageType } from "./ChatBox";
 export default function ChatInput({ setMessages }: { setMessages: Dispatch<SetStateAction<MessageType[]>> }) {
 
     const [userInput, setUserInput] = useState<string>('')
+    const [error, setError] = useState<null | Error>(null)
 
     function sendMessageByEnter(event: KeyboardEvent<HTMLTextAreaElement>) {
         if (event.shiftKey) {
@@ -29,23 +30,28 @@ export default function ChatInput({ setMessages }: { setMessages: Dispatch<SetSt
     }
 
     return (
-        <div className='border my-4 flex flex-col rounded-xl'>
-            <div className='grow flex'>
-
-                <textarea
-                    className='resize-none grow rounded-l-xl p-1 outline-0'
-                    value={userInput}
-                    onChange={(event) => setUserInput(event.currentTarget.value)}
-                    onKeyDown={(event) => sendMessageByEnter(event)}
-                >
-                </textarea>
-                <button
-                    className='bg-(--banner-neutral) dark:bg-(--banner-neutral-dark) w-10 rounded-r-xl flex justify-center items-center hover:opacity-70'
-                    onClick={() => sendMessage()}
-                >
-                    <MdSend />
-                </button>
+        <>
+            {
+                error &&
+                <div className='text-red-500 -mb-4'>{error.message}</div>
+            }
+            <div className='border my-4 flex flex-col rounded-xl'>
+                <div className='grow flex'>
+                    <textarea
+                        className='resize-none grow rounded-l-xl p-1 outline-0'
+                        value={userInput}
+                        onChange={(event) => setUserInput(event.currentTarget.value)}
+                        onKeyDown={(event) => sendMessageByEnter(event)}
+                    >
+                    </textarea>
+                    <button
+                        className='bg-(--banner-neutral) dark:bg-(--banner-neutral-dark) w-10 rounded-r-xl flex justify-center items-center hover:opacity-70'
+                        onClick={() => sendMessage()}
+                    >
+                        <MdSend />
+                    </button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
